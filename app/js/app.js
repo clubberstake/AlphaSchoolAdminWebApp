ba7693c18bbf80a809a408d4f73161991d09e7a6
clubberstake <gregory.lang@gmail.com>
2015-11-03 18:15:52 -0500

    Adding simple home page

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,11 @@
 var app = angular.module('alphaAdminWebApp', ['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/home', {
+		templateUrl: 'app/js/home/home.html',
+		controller: 'HomeController'
+	})
+		.otherwise({
+            redirectTo: '/home'
+        });
+}]);
