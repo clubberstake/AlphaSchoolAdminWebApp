1ab53ead1f7861ddeb6e4d5474c2071f97a6c7a6
Lang <ek0a6xw@na.corp.mckesson.com>
2016-04-19 20:29:40 -0400

    sample angular bindings

--- app/js/adminAddCourse/adminAddCourse.js
+++ app/js/adminAddCourse/adminAddCourse.js
@@ -1,35 +1,79 @@
 app.controller('AdminAddCourseController', ['$scope', '$location', 'AdminAddCourseService',
-    function($scope, $location, AdminAddCourseService) {
+    function ($scope, $location, AdminAddCourseService) {
 
 
-        $scope.navigateAdminAssignAccount = function() {
+        $scope.navigateAdminAssignAccount = function () {
 
             $location.path('/adminAssignAccount');
 
         };
 
-        $scope.navigateAdminAssignAccount = function() {
+        $scope.navigateAdminAssignAccount = function () {
 
             $location.path('/adminAddAccount');
 
         };
-        AdminAddCourseService.get().then(function(responseObj) {
+
+        $scope.courseList = [];
+        $scope.selectedCourse = '';
+
+        AdminAddCourseService.get().then(function (responseObj) {
             console.log('Attempting adminAddCourse get');
             console.log(responseObj.data);
+            $scope.courseList = [
+                {
+                    grade: {
+                        name: 'Grade 1',
+                        courses: [
+                            {
+                                courseName: "Advanced English"
+                            },
+                            {
+                                courseName: "Advanced Spanish"
+                            },
+                            {
+                                courseName: "Advanced Italian"
+                            },
+                            {
+                                courseName: "Advanced Arabic"
+                            }]
+                    }
+                },
+                {
+                    grade: {
+                        name: 'Grade 2',
+                        courses: [
+                            {
+                                courseName: "Advanced Math"
+                            },
+                            {
+                                courseName: "Advanced Biology"
+                            },
+                            {
+                                courseName: "Advanced Chemistry"
+                            },
+                            {
+                                courseName: "Advanced Physics"
+                            }]
+                    }
+                },
+            ];
+            console.log($scope.courseList[0].grade);
         });
-        AdminAddCourseService.post().then(function(responseObj) {
+
+        AdminAddCourseService.post().then(function (responseObj) {
             console.log('Attempting adminAddCoursePostScreenData post');
             console.log(responseObj.data);
         });
     }]);
-app.factory('AdminAddCourseService', ['$http', 'UrlService', function($http, UrlService) {
+app.factory('AdminAddCourseService', ['$http', 'UrlService', function ($http, UrlService) {
 
-    var getData = function() {
+    var getData = function () {
         return UrlService.adminAddCourseGetScreenData();
     };
 
-    var postCourse = function() {
-        var course = { "ID": "987654321", "CourseName": "Operating Systems", "Grade": 1}
+    var postCourse = function () {
+        var course = { "ID": "987654321", "CourseName": "Operating Systems", "Grade": 1 }
         return UrlService.adminAddCoursePostScreenData(course);
     };
     return {
--- app/js/adminAssignCourse/adminAssignCourse.js
+++ app/js/adminAssignCourse/adminAssignCourse.js
@@ -1,5 +1,9 @@
 app.controller('AdminAssignCourseController', ['$scope', '$location',
     function ($scope, $location) {
-        $scope.courseList = [{ name: 'Science' }, { name: 'Math' }, { name: 'Spelling' }, { name: 'Reading' }];
-        $scope.selectedCourse = '';        
+        $scope.courseList = [
+            { name: 'Science' },
+            { name: 'Math' },
+            { name: 'Spelling' },
+            { name: 'Reading' }];
+        $scope.selectedCourse = '';
     }]);
